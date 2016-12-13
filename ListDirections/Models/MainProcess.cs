180f5218d90502170f7f93c52b5739e1b1aa0e1b
patrik1109 <patrikeevegor@gmail.com>
2016-12-13 17:55:00 +0200

    111111
    111111

---
+++ ListDirections/Models/MainProcess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ListDirections.Models
+{
+    public class MainProcess
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string WorkingDir { get; set; }
+        public string FinalScript { get; set; }
+    }
+}
